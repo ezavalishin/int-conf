89dcb7dea2224b5733793ef3bf945037d9de2551
Evgeniy Zavalishin <ezavalishin@gmail.com>
2020-11-07 23:43:31 +0400

    new

--- src/pages/OneFilePlayer.tsx
+++ src/pages/OneFilePlayer.tsx
@@ -2,9 +2,18 @@ import React, {useEffect, useMemo, useRef, useState} from 'react';
 import {Box, FormControl, InputLabel, MenuItem, Select} from '@material-ui/core';
 import ReactPlayer from 'react-player';
 
+const videos = [
+  '/videos/new/kitchen_open_dark',
+  '/videos/new/kitchen_closed_dark',
+  '/videos/new/kitchen_open_light',
+  '/videos/new/kitchen_closed_light',
+];
+
 const OneFilePlayer = () => {
 
+
   const player = useRef<ReactPlayer>(null);
+  const [format, setFormat] = useState('mp4');
 
   const timeRef = useRef(0);
 
@@ -16,6 +25,48 @@ const OneFilePlayer = () => {
   const [playing, setPlaying] = useState(false);
   const [loading, setLoading] = useState(true);
 
+  const [windowIsReady, setWindowIsReady] = useState(false);
+
+  useEffect(() => {
+    const video = document.createElement('video');
+
+    console.log(videos);
+
+    const webmSupported = video.canPlayType('video/webm');
+
+    let f = 'mp4';
+    if (webmSupported !== '') {
+      setFormat('webm');
+      f = 'webm';
+    }
+
+    const promises = [];
+
+    for(let i = 0; i < videos.length; i++) {
+      const p = new Promise((resolve) => {
+        const video = videos[i] + '.' + f;
+
+
+        const req = new XMLHttpRequest();
+
+        req.open('GET', video, true);
+
+        req.addEventListener('load', () => {
+          resolve(true);
+        });
+
+        req.send();
+      });
+
+      promises.push(p);
+    }
+
+    Promise.all(promises).then(() => {
+      setPlaying(true);
+      setWindowIsReady(true);
+    });
+  }, []);
+
   // const [currentTIme, setCurrentTime] = useState(0);
 
   const seekToCurrent = () => {
@@ -26,7 +77,10 @@ const OneFilePlayer = () => {
 
   useEffect(() => {
     if (player.current) {
+      console.log(player.current.getInternalPlayer());
       const htmlVideo = player.current.getInternalPlayer() as HTMLVideoElement;
+
+      console.log('video', htmlVideo);
 
       if (htmlVideo) {
         htmlVideo.addEventListener('loadedmetadata', () => {
@@ -36,7 +90,7 @@ const OneFilePlayer = () => {
 
       }
     }
-  }, [player.current]);
+  }, [player.current, playing]);
 
   const makeScreen = () => {
     const canvas = document.createElement('canvas');
@@ -45,6 +99,9 @@ const OneFilePlayer = () => {
     const ctx = canvas.getContext('2d');
 
     if (player.current && ctx) {
+      if (!player.current.getInternalPlayer()) {
+        return;
+      }
       ctx.drawImage(player.current.getInternalPlayer() as HTMLVideoElement, 0, 0, canvas.width, canvas.height);
 
       const dataURI = canvas.toDataURL('image/jpeg');
@@ -56,34 +113,38 @@ const OneFilePlayer = () => {
 
   const videoUrl = useMemo(() => {
 
+    console.log(format, type, floor);
+
+
     makeScreen();
+
 
     setLoading(true);
 
-    return `/videos/new/kitchen_${type}_${floor}.mp4`;
-  }, [type, floor]);
+    return `/videos/new/kitchen_${type}_${floor}.${format}`;
+  }, [type, floor, format]);
 
   const handleReady = () => {
     setLoading(false);
-    setPlaying(true);
   };
 
 
   return (
     <Box display="flex" minHeight="100vh" alignItems="center" justifyContent="center">
+
       <Box>
         <Box display="flex">
           <Box mx={1}>
             <FormControl>
               <InputLabel>
-                Тип
+                  Тип
               </InputLabel>
               <Select value={type} onChange={(e) => setType(e.target.value as string)}>
                 <MenuItem value="open">
-                  Открытый
+                    Открытый
                 </MenuItem>
                 <MenuItem value="closed">
-                  Закрытый
+                    Закрытый
                 </MenuItem>
               </Select>
             </FormControl>
@@ -91,20 +152,21 @@ const OneFilePlayer = () => {
           <Box mx={1}>
             <FormControl>
               <InputLabel>
-                Пол
+                  Пол
               </InputLabel>
               <Select value={floor} onChange={(e) => setFloor(e.target.value as string)}>
                 <MenuItem value="light">
-                  Светлый
+                    Светлый
                 </MenuItem>
                 <MenuItem value="dark">
-                  Темный
+                    Темный
                 </MenuItem>
               </Select>
             </FormControl>
           </Box>
         </Box>
         <Box mt={1} position="relative">
+          {!windowIsReady && 'loading'}
           <ReactPlayer
             onProgress={({playedSeconds}) => {
               if (playedSeconds > 0) {
@@ -126,6 +188,8 @@ const OneFilePlayer = () => {
 
         </Box>
       </Box>
+
+
     </Box>
   );
 };
